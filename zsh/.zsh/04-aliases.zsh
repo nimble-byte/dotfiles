a01349e315a59f70537eff978df75f4648ca6088
Til Goepfert <tilgoepfert@gmail.com>
2019-05-10 16:17:22 +0200

    add magic local git pruning

--- zsh/.zsh/04-aliases.zsh
+++ zsh/.zsh/04-aliases.zsh
@@ -8,6 +8,7 @@ alias gaa="git add --all"
 alias gc="git commit -m"
 alias gca="git commit -am"
 alias gco="git checkout"
+alias gpl="git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
 # L
 alias ll="ls -alF"
 # R
