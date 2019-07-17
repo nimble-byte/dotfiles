c352e55fa53b27a7052eb6d01a7cdd4eb5bf529b
Til Goepfert <til@linux.fritz.box>
2019-07-17 20:14:09 +0200

    WIP prompt rework

---
+++ initial-setup/oh-my-zsh.sh
@@ -0,0 +1,9 @@
+# Install oh-my-zsh and plugins
+# Oh-My-Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Powerlevel10K
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# ZSH-Autosuggestionsi
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
