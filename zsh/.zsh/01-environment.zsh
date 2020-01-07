b98ae5f90a81fc7a6cb8b38c1944833bd597661e
Til Goepfert <tilgoepfert@gmail.com>
2020-01-08 00:58:21 +0100

    added snapd; adjusted extension settings for vscode

--- zsh/.zsh/01-environment.zsh
+++ zsh/.zsh/01-environment.zsh
@@ -10,3 +10,6 @@ export UPDATE_ZSH_DAYS=6
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
+
+# PATH
+export PATH="$PATH:/snap/bin"
