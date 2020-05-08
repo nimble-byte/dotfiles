90ae464519b7400b58ac96a7d1853e7ad9ea7d17
Til Goepfert <tilgoepfert@gmail.com>
2020-05-08 22:23:25 +0200

    keep default unicode characters for git status

--- zsh/.zsh/02-functions.zsh
+++ zsh/.zsh/02-functions.zsh
@@ -1,92 +1,86 @@
 function git_formatter() {
-  emulate -L zsh
+    emulate -L zsh
 
-  if [[ -n $P9K_CONTENT ]]; then
-    # If P9K_CONTENT is not empty, use it. It's either "loading" or from vcs_info (not from
-    # gitstatus plugin). VCS_STATUS_* parameters are not available in this case.
-    typeset -g git_format=$P9K_CONTENT
-    return
-  fi
+    if [[ -n $P9K_CONTENT ]]; then
+      # If P9K_CONTENT is not empty, use it. It's either "loading" or from vcs_info (not from
+      # gitstatus plugin). VCS_STATUS_* parameters are not available in this case.
+      typeset -g git_format=$P9K_CONTENT
+      return
+    fi
 
-  if (( $1 )); then
-    # Styling for up-to-date Git status.
-    local       meta='%f'     # default foreground
-    local      clean='%76F'   # green foreground
-    local   modified='%178F'  # yellow foreground
-    local  untracked='%39F'   # blue foreground
-    local conflicted='%196F'  # red foreground
-  else
-    # Styling for incomplete and stale Git status.
-    local       meta='%244F'  # grey foreground
-    local      clean='%244F'  # grey foreground
-    local   modified='%244F'  # grey foreground
-    local  untracked='%244F'  # grey foreground
-    local conflicted='%244F'  # grey foreground
-  fi
+    if (( $1 )); then
+      # Styling for up-to-date Git status.
+      local       meta='%f'     # default foreground
+      local      clean='%76F'   # green foreground
+      local   modified='%178F'  # yellow foreground
+      local  untracked='%39F'   # blue foreground
+      local conflicted='%196F'  # red foreground
+    else
+      # Styling for incomplete and stale Git status.
+      local       meta='%244F'  # grey foreground
+      local      clean='%244F'  # grey foreground
+      local   modified='%244F'  # grey foreground
+      local  untracked='%244F'  # grey foreground
+      local conflicted='%244F'  # grey foreground
+    fi
 
-  local res
-  local where  # branch or tag
-  if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
-    res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}"
-    where=${(V)VCS_STATUS_LOCAL_BRANCH}
-  elif [[ -n $VCS_STATUS_TAG ]]; then
-    res+="${meta}#"
-    where=${(V)VCS_STATUS_TAG}
-  fi
+    local res
+    local where  # branch or tag
+    if [[ -n $VCS_STATUS_LOCAL_BRANCH ]]; then
+      res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}"
+      where=${(V)VCS_STATUS_LOCAL_BRANCH}
+    elif [[ -n $VCS_STATUS_TAG ]]; then
+      res+="${meta}#"
+      where=${(V)VCS_STATUS_TAG}
+    fi
 
-  # If local branch name or tag is at most 32 characters long, show it in full.
-  # Otherwise show the first 12 … the last 12.
-  # Tip: To always show local branch name in full without truncation, delete the next line.
-  (( $#where > 32 )) && where[13,-13]="…"
-  res+="${clean}${where//\%/%%}"  # escape %
+    # If local branch name or tag is at most 32 characters long, show it in full.
+    # Otherwise show the first 12 … the last 12.
+    # Tip: To always show local branch name in full without truncation, delete the next line.
+    (( $#where > 32 )) && where[13,-13]="…"
+    res+="${clean}${where//\%/%%}"  # escape %
 
-  # Display the current Git commit if there is no branch or tag.
-  # Tip: To always display the current Git commit, remove `[[ -z $where ]] &&` from the next line.
-  [[ -z $where ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
+    # Display the current Git commit if there is no branch or tag.
+    # Tip: To always display the current Git commit, remove `[[ -z $where ]] &&` from the next line.
+    [[ -z $where ]] && res+="${meta}@${clean}${VCS_STATUS_COMMIT[1,8]}"
 
-  # Show tracking branch name if it differs from local branch.
-  if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
-    res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"  # escape %
-  fi
+    # Show tracking branch name if it differs from local branch.
+    if [[ -n ${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH} ]]; then
+      res+="${meta}:${clean}${(V)VCS_STATUS_REMOTE_BRANCH//\%/%%}"  # escape %
+    fi
 
-  # ⇣42 if behind the remote.
-  # (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}${(g::)POWERLEVEL9K_VCS_COMMITS_AHEAD_ICON} ${VCS_STATUS_COMMITS_BEHIND}"
-  (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}${(g::)POWERLEVEL9K_VCS_COMMITS_BEHIND_ICON}"
-  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
-  (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
-  # (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}${(g::)POWERLEVEL9K_VCS_COMMITS_AHEAD_ICON} ${VCS_STATUS_COMMITS_AHEAD}"
-  (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}${(g::)POWERLEVEL9K_VCS_COMMITS_AHEAD_ICON}"
-  # ⇠42 if behind the push remote.
-  # (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
-  # (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
-  # # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
-  # (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-  # *42 if have stashes.
-  # (( VCS_STATUS_STASHES        )) && res+=" ${clean}${(g::)POWERLEVEL9K_VCS_STASH_ICON} ${VCS_STATUS_STASHES}"
-  (( VCS_STATUS_STASHES        )) && res+=" ${clean}${(g::)POWERLEVEL9K_VCS_STASH_ICON}"
-  # 'merge' if the repo is in an unusual state.
-  [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
-  # ~42 if have merge conflicts.
-  # (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}${(g::)POWERLEVEL9K_VCS_CONFLICT_ICON} ${VCS_STATUS_NUM_CONFLICTED}"
-  (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}${(g::)POWERLEVEL9K_VCS_CONFLICT_ICON}"
-  # +42 if have staged changes.
-  # (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}${(g::)POWERLEVEL9K_VCS_MODIFIED_ICON} ${VCS_STATUS_NUM_STAGED}"
-  (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}${(g::)POWERLEVEL9K_VCS_MODIFIED_ICON}"
-  # !42 if have unstaged changes.
-  # (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}${(g::)POWERLEVEL9K_VCS_UNSTAGED_ICON} ${VCS_STATUS_NUM_UNSTAGED}"
-  (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}${(g::)POWERLEVEL9K_VCS_UNSTAGED_ICON}"
-  # ?42 if have untracked files. It's really a question mark, your font isn't broken.
-  # See POWERLEVEL9K_VCS_UNTRACKED_ICON above if you want to use a different icon.
-  # Remove the next line if you don't want to see untracked files at all.
-  # (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}${(g::)POWERLEVEL9K_VCS_UNTRACKED_ICON} ${VCS_STATUS_NUM_UNTRACKED}"
-  (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}${(g::)POWERLEVEL9K_VCS_UNTRACKED_ICON}"
-  # "─" if the number of unstaged files is unknown. This can happen due to
-  # POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY (see below) being set to a non-negative number lower
-  # than the number of files in the Git index, or due to bash.showDirtyState being set to false
-  # in the repository config. The number of staged and untracked files may also be unknown
-  # in this case.
-  (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
+    # ⇣42 if behind the remote.
+    (( VCS_STATUS_COMMITS_BEHIND )) && res+=" ${clean}⇣${VCS_STATUS_COMMITS_BEHIND}"
+    # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42.
+    (( VCS_STATUS_COMMITS_AHEAD && !VCS_STATUS_COMMITS_BEHIND )) && res+=" "
+    (( VCS_STATUS_COMMITS_AHEAD  )) && res+="${clean}⇡${VCS_STATUS_COMMITS_AHEAD}"
+    # ⇠42 if behind the push remote.
+    (( VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" ${clean}⇠${VCS_STATUS_PUSH_COMMITS_BEHIND}"
+    (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
+    # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
+    (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
+    # *42 if have stashes.
+    (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
+    # 'merge' if the repo is in an unusual state.
+    [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
+    # ~42 if have merge conflicts.
+    (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
+    # +42 if have staged changes.
+    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
+    # !42 if have unstaged changes.
+    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+    # ?42 if have untracked files. It's really a question mark, your font isn't broken.
+    # See POWERLEVEL9K_VCS_UNTRACKED_ICON above if you want to use a different icon.
+    # Remove the next line if you don't want to see untracked files at all.
+    # (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}${(g::)POWERLEVEL9K_VCS_UNTRACKED_ICON}${VCS_STATUS_NUM_UNTRACKED}"
+    (( VCS_STATUS_NUM_UNTRACKED  )) && res+=" ${untracked}?${VCS_STATUS_NUM_UNTRACKED}"
+    # "─" if the number of unstaged files is unknown. This can happen due to
+    # POWERLEVEL9K_VCS_MAX_INDEX_SIZE_DIRTY (see below) being set to a non-negative number lower
+    # than the number of files in the Git index, or due to bash.showDirtyState being set to false
+    # in the repository config. The number of staged and untracked files may also be unknown
+    # in this case.
+    (( VCS_STATUS_HAS_UNSTAGED == -1 )) && res+=" ${modified}─"
 
-  typeset -g git_format=$res
-}
-functions -M git_formatter 2>/dev/null
+    typeset -g git_format=$res
+  }
+  functions -M git_formatter 2>/dev/null
